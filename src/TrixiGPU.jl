31f9a957ffba0ef587d1ef7f77c38e8a3f33a1d1
huiyuxie <huiyuxie.sde@gmail.com>
2024-07-30 13:33:38 -1000

    Add basic packages

--- src/TrixiGPU.jl
+++ src/TrixiGPU.jl
@@ -1,5 +1,7 @@
 module TrixiGPU
 
+using Trixi: AbstractEquations
+
 export greet_your_package_name
 include("function.jl")
 
--- src/function.jl
+++ src/function.jl
@@ -1,3 +1,7 @@
 function greet_your_package_name()
     return "Hello YourPackageName!"
 end
+
+struct MyEquations{T} <: AbstractEquations{1, T}
+    # Add your fields here
+end
