4c671838e6a5e64c86388c0bdaa3a0a89936d78b
huiyuxie <huiyuxie.sde@gmail.com>
2024-07-31 00:20:40 -1000

    Test kernel configurators

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,5 @@ using TrixiGPU
 using Test
 
 @testset "TrixiGPU.jl" begin
-    @test TrixiGPU.greet_your_package_name() == "Hello YourPackageName!"
-    @test TrixiGPU.greet_your_package_name() != "Hello world!"
+    include("test_auxiliary.jl")
 end
